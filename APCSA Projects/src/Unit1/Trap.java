806d72c4acbdcf9f2f35b606935fa95f7471d2a2
mckenziegarandm1677 <mckenziegarandm1677@CA-SU-F106-38.sduhsd.lan>
2020-02-03 10:38:58 -0800

    all asignments completed in class

---
+++ APCSA Projects/src/Unit1/Trap.java
@@ -0,0 +1,16 @@
+package Unit1;
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		double bases =   base1 + base2;
+		double basesM = .5*(bases)*height;
+		return basesM;
+	}
+}
