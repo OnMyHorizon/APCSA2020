bbc6fef9e430c875e6f754e625c83512cb40de69
mckenziegarandm1677 <mckenziegarandm1677@CA-SU-F106-38.sduhsd.lan>
2020-02-13 10:18:53 -0800

    this is the project

---
+++ APCSA Projects/src/ConsumerReviewProject/Main.java
@@ -0,0 +1,22 @@
+package ConsumerReviewProject;
+
+import java.util.Scanner;
+import java.io.File;
+
+class Main {
+	public static void main(String[] args) {
+		// test reading from files
+		// read in the positive adjectives in postiveAdjectives.txt
+		try {
+			Scanner input = new Scanner(new File("src/ConsumerReviewProject/positiveAdjectives.txt"));
+			while (input.hasNextLine()) {
+				String temp = input.nextLine().trim();
+				System.out.println(temp);
+
+			}
+			input.close();
+		} catch (Exception e) {
+			System.out.println("Error reading or parsing postitiveAdjectives.txt\n" + e);
+		}
+	}
+}
